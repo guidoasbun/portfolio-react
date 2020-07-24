a85ea94772cb0ce61134e6f2eb549864e66b93cc
guidoasbun <grdodrigo8@gmail.com>
2020-07-23 23:27:43 -0700

    About Me

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -22,6 +22,10 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 
+import AboutMe from "../AboutMe";
+import Skills from "../Skills";
+
+
 import ProfileImage from "../ProfileImage";
 
 const drawerWidth = 210;
@@ -39,8 +43,8 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     [theme.breakpoints.up("sm")]: {
       width: `calc(100% - ${drawerWidth}px)`,
+      height: '50px',
       marginLeft: drawerWidth,
-
     },
   },
   menuButton: {
@@ -58,6 +62,9 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  card: {
+    marginBottom: '20px'
+  }
 }));
 
 function Menu(props) {
@@ -108,7 +115,6 @@ function Menu(props) {
     <div>
       <ProfileImage/>
       <Divider />
-
       <List>
         {menuItemTop.map((item, index) => (
           <ListItem button key={item.text}>
@@ -117,7 +123,6 @@ function Menu(props) {
           </ListItem>
         ))}
       </List>
-
       <Divider />
       <List>
         {menuItemBottom.map((item, index) => (
@@ -149,9 +154,7 @@ function Menu(props) {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap>
-            Guido Asbun
-          </Typography>
+
         </Toolbar>
       </AppBar>
       <nav className={classes.drawer} aria-label="mailbox folders">
@@ -187,36 +190,9 @@ function Menu(props) {
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Typography variant="h2" noWrap align='center' gutterBottom> Guido Asbun </Typography>
+        <AboutMe/>
+        <Skills/>
       </main>
     </div>
   );

c2cff0410d6727283709e73df1022c254d40dc7e
Guido Asbun <gdrodrigo8@gmail.com>
2021-04-04 18:27:06 -0700

    Added weather dashboard and fire finder

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -7,7 +7,7 @@ export default [
     summary:
       "A full stack web application to showcase my information and projects",
     techUsed:
-      "Node, Express, MongoDB, HTTP/CSS, AWS (Elastic Beanstalk, CodeBuild, EC2, Load Balancer, Route 53, S3 Bucket)",
+      "React JS, HTTP/CSS, AWS (Elastic Beanstalk, CodeBuild, EC2, Load Balancer, Route 53, S3 Bucket)",
   },
   {
     title: "CrypIt",
@@ -28,12 +28,19 @@ export default [
     techUsed: "mySQL, Express, React Views, Node, JavaScript",
   },
   {
-    title: "Coder Book",
-    image: "https://guido-asbun.s3.amazonaws.com/Chuck-coderbook.gif",
-    githubLink: "https://github.com/guidoasbun/Coder-Book",
-    deployLink: "https://enigmatic-wildwood-51842.herokuapp.com/",
-    summary:
-      "A social media web application for coders to communicate, get latest tech news, do coding challenges, and test out HTML code.",
-    techUsed: "MongoDB, Express, React, Node, Passport, Material UI",
+    title: "Weather Dashboard",
+    image: "https://guido-asbun.s3.amazonaws.com/Weather+Dashboard.png",
+    githubLink: "https://github.com/guidoasbun/WeatherDashboard",
+    deployLink: "https://master.d2o5gk0ja3q3pq.amplifyapp.com/",
+    summary: "An application that searches and displays weather data.",
+    techUsed: "JavaScript, HTML, CSS, openweathermap.org api",
   },
+  {
+    title: "Weather Dashboard",
+    image: "https://guido-asbun.s3.amazonaws.com/WildFire+Tracker.png",
+    githubLink: "https://github.com/guidoasbun/wildfire-tracker-react",
+    deployLink: "https://master.d1xmvgmnxpwo2a.amplifyapp.com/",
+    summary: "A web application that displays current wildfire data laid out on google maps.",
+    techUsed: "React JS, HTML, CSS, Google maps, NASA api",
+  }
 ];

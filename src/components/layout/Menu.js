f7e60c787cb1e6716d76281d0a90aa090a4081c2
GuidoAsbun <guido@asbun.io>
2022-01-07 00:01:22 -0800

    Updated Resume

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -123,7 +123,7 @@ function Menu(props) {
     {
       text: "Resume",
       icon: <DescriptionIcon />,
-      link: "https://guido-asbun.s3.amazonaws.com/Resume+1.23+-+July+2021.pdf",
+      link: "https://guido-asbun.s3.amazonaws.com/Guido+Asbun+Resume+V1.pdf",
     },
   ];
 
